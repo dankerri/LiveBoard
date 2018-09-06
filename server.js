6d4131971ddfbaa2304a0274f9fc8bd3b80f9b34
danker <itrrion@gmail.com>
2018-09-07 00:06:20 +0800

    multple room support

--- server.js
+++ server.js
@@ -3,21 +3,20 @@ var io = require('socket.io')();
 io.on('connection', client=>{
 	// Search room Side
 	client.on('check', room=>{
-		console.log('check ' + room);
-
+		console.log('check ' + room );
 		if ( io.sockets.adapter.rooms[room] === undefined ) {
 			client.emit('unexist', 'THE '+ room +' DOSEN\'T CREATE YET');
-			console.log(io.sockets.adapter.rooms)			
+			// console.log(io.sockets.adapter.rooms)			
 		}
 		else { 
-			client.emit('exist');
+			client.emit('exist'); 
 		}
 	});
 
 	// LiveBoard side
 	client.on('addUser', room =>{
 		client.join(room);
-		console.log("Add a USER");
+		console.log(room + " Add a USER");
 	})
 
 
@@ -33,9 +32,10 @@ io.on('connection', client=>{
 
 	client.on('updateCode', code=>{
 		io.sockets.in(code.room).emit('newCode', code.code);
-		console.log("GET "+code.room+" UPDATE");
+		console.log("GET "+code.room+" UPDATE " + code.code);
 	});
 });
 
 io.listen(5000);
 console.log('port 5000')
+
