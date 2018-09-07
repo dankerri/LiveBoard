60e079308387deff6136e08e5ef9fe6ebec2cf69
danker <itrrion@gmail.com>
2018-09-07 11:51:43 +0800

    changed the config

--- client/src/components/config.js
+++ client/src/components/config.js
@@ -1,6 +1,7 @@
-const address = 'http://192.168.0.107';
-const port = 5000;
+const ADDRESS = 'http://207.148.102.115';
+// const address = 'http://192.168.0.107';
+const PORT = 5000;
 
-const THE_URL = address + ':' + port 
+const THE_URL = ADDRESS + ':' + PORT 
 
 export default  THE_URL;
